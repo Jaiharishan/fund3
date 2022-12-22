5d3e3bc6810075c796f658babc33b859cf3c382f
Jaiharishan <downdkar@gmail.com>
2022-12-22 20:13:10 +0530

    Add: Added Navbar with Solana Wallet Button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import WalletContextProvider from "../helpers/WalletContextProvider";
+import { Button } from "@chakra-ui/react";
+
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1>Home!</h1>
+      <Navbar />
     </>
   );
 }
